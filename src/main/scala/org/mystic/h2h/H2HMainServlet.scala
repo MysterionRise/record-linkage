67af3821982f88f12b42cce989fe28371c287cf8
Mysterion <pericov.ko@gmail.com>
2013-09-10 15:36:14 +0400

    Add list of fantasy games

--- src/main/scala/org/mystic/h2h/H2HMainServlet.scala
+++ src/main/scala/org/mystic/h2h/H2HMainServlet.scala
@@ -1,15 +1,27 @@
 package org.mystic.h2h
 
-import org.scalatra._
-import scalate.ScalateSupport
 
 class H2HMainServlet extends H2hTestAppStack {
 
   get("/h2h-nba") {
     <html>
       <body>
-        <h1>This is main page for NBA H2H on sports.ru</h1>
-        Новый сезон NBA еще не начался, так что пока еще есть время для подготовки. А так же чествования чемпиона H2H NBA 2012/2013 - <a href="http://www.sports.ru/profile/28147397/">tan01</a>
+        <h2>This is main page for NBA H2H on sports.ru</h2>
+        Новый сезон NBA еще не начался, так что пока еще есть время для подготовки. А так же чествования чемпиона H2H NBA 2012/2013 -
+        <a href="http://www.sports.ru/profile/28147397/">tan01</a>
+      </body>
+    </html>
+  }
+
+  get("/") {
+    <html>
+      <body>
+        <h2>List of current H2H Fantasy Games</h2>
+        <ul>
+          <li>
+            <a href="/h2h-nba">H2H NBA 2013/2014</a>
+          </li>
+        </ul>
       </body>
     </html>
   }
