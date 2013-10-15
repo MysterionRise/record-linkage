991a2395e32413e30c988994e5f0c2a94752598d
Mysterion <pericov.ko@gmail.com>
2013-10-16 00:04:44 +0400

    Try to fix heroku deploy

--- project/build.scala
+++ project/build.scala
@@ -1,7 +1,6 @@
 import sbt._
 import Keys._
 import org.scalatra.sbt._
-import org.scalatra.sbt.PluginKeys._
 import com.mojolly.scalate.ScalatePlugin._
 import ScalateKeys._
 
@@ -12,7 +11,7 @@ object H2hTestAppBuild extends Build {
   val ScalaVersion = "2.10.2"
   val ScalatraVersion = "2.2.1"
 
-  lazy val project = Project (
+  lazy val project = Project(
     "h2h-test-app",
     file("."),
     settings = seq(com.typesafe.startscript.StartScriptPlugin.startScriptForClassesSettings: _*) ++ Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
@@ -29,21 +28,22 @@ object H2hTestAppBuild extends Build {
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
         "ch.qos.logback" % "logback-classic" % "1.0.11" % "runtime",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
         "org.mongodb" %% "casbah" % "2.5.0",
-        "com.github.nscala-time" %% "nscala-time" % "0.6.0"
+        "com.github.nscala-time" %% "nscala-time" % "0.6.0",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq(
-              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
-            Some("templates")
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) {
+        base =>
+          Seq(
+            TemplateConfig(
+              base / "webapp" / "WEB-INF" / "templates",
+              Seq.empty, /* default imports should be added here */
+              Seq(
+                Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+              ), /* add extra bindings here */
+              Some("templates")
+            )
           )
-        )
       }
     )
   )
