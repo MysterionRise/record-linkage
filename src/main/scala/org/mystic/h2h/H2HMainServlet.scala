7053ef4569eb8badcb9fe8c9708f36bd37f99315
Mysterion <pericov.ko@gmail.com>
2013-10-23 12:57:24 +0400

    Add div 2 of H2H NBA

--- src/main/scala/org/mystic/h2h/H2HMainServlet.scala
+++ src/main/scala/org/mystic/h2h/H2HMainServlet.scala
@@ -6,20 +6,24 @@ class H2HMainServlet extends H2hTestAppStack {
   get("/h2h-nba-div1") {
     contentType = "text/html"
     ssp("/league",
-      "layout" -> "WEB-INF/templates/layouts/khl.ssp",
-      "leagueName" -> "NBA",
+      "layout" -> "WEB-INF/templates/layouts/h2h.ssp",
+      "leagueName" -> "NBA Div 1",
       "leagueURI" -> "http://www.sports.ru/fantasy/basketball/league/9826.html")
   }
 
 
   get("/h2h-nba-div2") {
-    <h1>Not found. Bummer.</h1>
+    contentType = "text/html"
+    ssp("/league",
+      "layout" -> "WEB-INF/templates/layouts/h2h.ssp",
+      "leagueName" -> "NBA Div 2",
+      "leagueURI" -> "http://www.sports.ru/fantasy/basketball/league/9831.html")
   }
 
   get("/h2h-khl") {
     contentType = "text/html"
     ssp("/khl",
-      "layout" -> "WEB-INF/templates/layouts/khl.ssp",
+      "layout" -> "WEB-INF/templates/layouts/h2h.ssp",
       "pageSize" -> 50,
       "currentPage" -> 0,
       "uri" -> "/h2h-khl/",
@@ -30,7 +34,7 @@ class H2HMainServlet extends H2hTestAppStack {
   get("/h2h-khl/:id") {
     contentType = "text/html"
     ssp("/khl",
-      "layout" -> "WEB-INF/templates/layouts/khl.ssp",
+      "layout" -> "WEB-INF/templates/layouts/h2h.ssp",
       "pageSize" -> 50,
       "currentPage" -> Integer.parseInt(params("id")),
       "uri" -> "/h2h-khl/",
