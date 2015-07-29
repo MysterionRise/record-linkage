ad333aaf6ca91d43cb3f864f064491e934fe7944
Konstantin Perikov <kperikov@griddynamics.com>
2015-07-29 22:41:56 +0200

    make it works with api

--- src/main/scala/ProBasketballAPI.scala
+++ src/main/scala/ProBasketballAPI.scala
@@ -1,10 +1,23 @@
 import dispatch.Defaults._
 import dispatch._
 
-object ProBasketballAPI extends App {
-   val svc = url("http://api.hostip.info/country.php")
-   val country = Http(svc OK as.String)
-    for (c <- country)
-      println(c)
+import scala.io.StdIn._
 
- }
+object ProBasketballAPI extends App {
+  val api_key = readLine()
+  val api = url("https://probasketballapi.com")
+  val params = Map("api_key" -> api_key)
+  val headers = Map("Content-type" -> "application/json")
+  val req = api / "teams" <<? params <:< headers
+  // need to do like that, cause probasketball api is bad with SSL
+  val http = Http.configure(_.setAcceptAnyCertificate(true))
+  val response = http(req.POST OK as.String).either
+  println(response)
+  response() match {
+    case Right(content) => println("Content: " + content)
+    case Left(StatusCode(404)) => println("Not found")
+    case Left(StatusCode(code)) => println("Some other code: " + code.toString)
+    case Left(e: Exception) => e.printStackTrace()
+  }
+  Http.shutdown()
+}
