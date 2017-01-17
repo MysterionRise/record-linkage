d7d7a9e3b17e9677b4b8eda1562f09bedebb814f
Konstantin Perikov <pericov.ko@gmail.com>
2017-01-17 12:05:14 +0300

    add more stuff

--- src/main/scala/org/mystic/producer/MeetupStreamProducer.scala
+++ src/main/scala/org/mystic/producer/MeetupStreamProducer.scala
@@ -1,9 +1,29 @@
 package org.mystic.producer
 
-class MeetupStreamProducer {
+import java.util.Properties
 
+import org.apache.kafka.clients.producer.ProducerRecord
 
+import scalaj.http.Http
 
+case class MeetupStreamProducer(topicName: String, meetupURI: String, props: Properties) extends KafkaStreamProducer {
+  //"http://stream.meetup.com/2/rsvps"
 
+  /*val props = new Properties()
+   props.put("bootstrap.servers", "localhost:9092")
+
+   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+   val producer = new KafkaProducer[String, String](props)*/
+
+  override def runStream() = {
+    Http(meetupURI).execute(is => {
+      scala.io.Source.fromInputStream(is).getLines().foreach(lines => {
+        println(lines)
+        val record = new ProducerRecord(topicName, "key", s"$lines")
+        //producer.send(record)
+      })
+    })
+  }
 
 }
--- src/main/scala/org/mystic/producer/TwitterStreamProducer.scala
+++ src/main/scala/org/mystic/producer/TwitterStreamProducer.scala
@@ -1,7 +1,36 @@
 package org.mystic.producer
 
-class TwitterStreamProducer {
+import java.util.Properties
 
+import twitter4j._
+import twitter4j.auth.AccessToken
 
+case class TwitterStreamProducer(topicName: String, props: Properties, accessToken: AccessToken, consumerKey: String, consumerSecret: String) extends KafkaStreamProducer {
+
+  override def runStream() = {
+    val twitterStream = new TwitterStreamFactory().getInstance()
+    // from configs and from twitter profile
+    twitterStream.setOAuthConsumer(consumerKey, consumerSecret)
+    twitterStream.setOAuthAccessToken(accessToken)
+    twitterStream.addListener(listener)
+    // sample() method internally creates a thread which manipulates TwitterStream and calls these adequate listener methods continuously.
+    twitterStream.sample()
+  }
+
+  val listener = new StatusListener {
+    override def onStallWarning(warning: StallWarning) = ???
+
+    override def onDeletionNotice(statusDeletionNotice: StatusDeletionNotice) = ???
+
+    override def onScrubGeo(userId: Long, upToStatusId: Long) = ???
+
+    override def onStatus(status: Status) = {
+      println(status.getText)
+    }
+
+    override def onTrackLimitationNotice(numberOfLimitedStatuses: Int) = ???
+
+    override def onException(ex: Exception) = ???
+  }
 
 }
