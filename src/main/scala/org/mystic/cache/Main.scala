466bced925049e7157065607ae6ac005d3caddda
Konstantin Perikov <pericov.ko@gmail.com>
2017-01-31 13:09:50 +0300

    add stuff

--- src/main/scala/org/mystic/cache/Main.scala
+++ src/main/scala/org/mystic/cache/Main.scala
@@ -1,31 +1,13 @@
 package org.mystic.cache
 
-import com.hazelcast.config.Config
-import com.hazelcast.core.{Hazelcast, IQueue}
+import com.hazelcast.core.{Hazelcast, IMap, IQueue}
 
 
 object Main {
 
   def main(args: Array[String]): Unit = {
-    // try to get hazelcast.xml from classpath
-    val hz1 = Hazelcast.newHazelcastInstance();
-    val hz2 = Hazelcast.newHazelcastInstance();
-
-
-    /*val config = new Config()
-    config.setInstanceName("test")
-    val network = config.getNetworkConfig()
-    val join = network.getJoin()
-    join.getMulticastConfig().setEnabled(false)
-    val hz = Hazelcast.newHazelcastInstance(config)*/
-
-    val q1: IQueue[String] = hz1.getQueue("q")
-    q1.put("test1")
-
-    val q2: IQueue[String] = hz2.getQueue("q")
-    println(q2.poll())
-
-    hz1.shutdown()
-    hz2.shutdown()
+    val hz = Hazelcast.newHazelcastInstance()
+    val skus: IMap[Int, Int] = hz.getMap("skus")
+    
   }
 }
