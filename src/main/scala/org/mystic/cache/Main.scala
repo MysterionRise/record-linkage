1ee38fd80313558a4d2a04bf59aad2e2c8bb54d6
Konstantin Perikov <pericov.ko@gmail.com>
2017-02-08 15:50:48 +0300

    clean up stuff

--- src/main/scala/org/mystic/cache/Main.scala
+++ src/main/scala/org/mystic/cache/Main.scala
@@ -10,6 +10,25 @@ import scala.util.Random
 
 object Main {
 
+  def buySku(skus: IMap[Int, Int], sku: Int, rand: Random) = {
+    var skuToBuy = sku
+    var flag = true
+    while (flag) {
+      if (skus.containsKey(skuToBuy)) {
+        val oldValue = skus.get(skuToBuy)
+        val ammountToBuy = rand.nextInt(oldValue / 100 + 1)
+        val newValue = oldValue - ammountToBuy
+        if (newValue >= 0 && skus.replace(skuToBuy, oldValue, newValue)) {
+          println(s"buying $skuToBuy $ammountToBuy")
+          flag = false
+        }
+      } else {
+        skuToBuy = rand.nextInt(SKU_SIZE)
+        println(s"there is no available sku for key $skuToBuy")
+      }
+    }
+  }
+
   def main(args: Array[String]): Unit = {
 
     val rand = new Random()
@@ -19,43 +38,33 @@ object Main {
         val networkConfig = new ClientNetworkConfig().addAddress("10.132.0.2", "10.132.0.3", "10.132.0.4", "10.132.0.5")
         val clientConfig = new ClientConfig().setNetworkConfig(networkConfig)
         val hz = HazelcastClient.newHazelcastClient(clientConfig)
-        println(hz.getName)
         val skus: IMap[Int, Int] = hz.getMap("skus")
 
         while (true) {
-          var skuToBuy = rand.nextInt(SKU_SIZE)
-          var flag = true
-          while (flag) {
-            if (skus.containsKey(skuToBuy)) {
-              val oldValue = skus.get(skuToBuy)
-              val ammountToBuy = rand.nextInt(oldValue / 10 + 1)
-              val newValue = oldValue - ammountToBuy
-              if (newValue >= 0 && skus.replace(skuToBuy, oldValue, newValue)) {
-                println(s"buying $skuToBuy $ammountToBuy")
-                flag = false
-              }
-            } else {
-              skuToBuy = rand.nextInt(SKU_SIZE)
-              println(s"there is no available sku for key $skuToBuy")
-            }
+          if (rand.nextFloat() > 0.95f) {
+            val hotSku = rand.nextInt(SKU_SIZE / 100)
+            buySku(skus, hotSku, rand)
+            //hot sku case
+          } else {
+            val normalSku = rand.nextInt(SKU_SIZE)
+            buySku(skus, normalSku, rand)
+
           }
-          TimeUnit.MILLISECONDS.sleep(100)
+          TimeUnit.MILLISECONDS.sleep(10)
         }
       }
       case "write" => {
         val networkConfig = new ClientNetworkConfig().addAddress("10.132.0.2", "10.132.0.3", "10.132.0.4", "10.132.0.5")
         val clientConfig = new ClientConfig().setNetworkConfig(networkConfig)
         val hz = HazelcastClient.newHazelcastClient(clientConfig)
-        println(hz.getName)
         val skus: IMap[Int, Int] = hz.getMap("skus")
-
         while (true) {
           val skuToBuy = rand.nextInt(SKU_SIZE)
           var flag = true
           while (flag) {
             if (skus.containsKey(skuToBuy)) {
               val oldValue = skus.get(skuToBuy)
-              val ammountToAdd = rand.nextInt(oldValue / 10 + 1)
+              val ammountToAdd = rand.nextInt(oldValue / 100 + 1)
               val newValue = oldValue + ammountToAdd
               if (skus.replace(skuToBuy, oldValue, newValue)) {
                 println(s"adding $skuToBuy $ammountToAdd")
@@ -68,7 +77,16 @@ object Main {
               flag = false
             }
           }
-          TimeUnit.MILLISECONDS.sleep(100)
+          TimeUnit.MILLISECONDS.sleep(10)
+        }
+      }
+      case "init" => {
+        val networkConfig = new ClientNetworkConfig().addAddress("10.132.0.2", "10.132.0.3", "10.132.0.4", "10.132.0.5")
+        val clientConfig = new ClientConfig().setNetworkConfig(networkConfig)
+        val hz = HazelcastClient.newHazelcastClient(clientConfig)
+        val skus: IMap[Int, Int] = hz.getMap("skus")
+        for (i <- 0 to SKU_SIZE) {
+          skus.put(i, INIT_SIZE)
         }
       }
       case _ => {
--- src/main/scala/org/mystic/cache/package.scala
+++ src/main/scala/org/mystic/cache/package.scala
@@ -2,9 +2,6 @@ package org.mystic
 
 package object cache {
 
-  val INIT_SIZE = 1000
+  val INIT_SIZE = 10000
   val SKU_SIZE = 90000000
-  val AVAILABILITY_SIZE = 1000
-  val OPERATIONS = 100000
-  val BUYING_OPERATIONS = 50
 }
